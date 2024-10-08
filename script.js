a2eb695dbbd0304848eb26901d67a8ecefaf7ea3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-08 16:39:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 document.querySelector('.deploy-btn').addEventListener('click', function() {
     const repoLink = document.querySelector('.repo-link-input').value;
+    
+    // Check if the input is a valid GitHub link
+    if (!repoLink.startsWith("https://github.com/")) {
+        alert('Please input a valid GitHub repository URL.');
+        return;
+    }
+    
+    // Redirect to Heroku deployment if valid
     if (repoLink) {
         window.location.href = `https://heroku.com/deploy?template=${repoLink}`;
     } else {
