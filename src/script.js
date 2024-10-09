9e826be8c80984cc75c14acaa1b24015ee92ebe8
◄👨‍💻𝑴𝑼𝑹𝑨𝑳𝑰 ✽ 𝑩𝑶𝑻𝒁 ꨄ︎ <148250637+MuraliBotz@users.noreply.github.com>
2024-10-09 22:39:25 +0530

    Some Upgrades

--- src/script.js
+++ src/script.js
@@ -3,38 +3,41 @@ document.addEventListener("DOMContentLoaded", () => {
   const repoLinkInput = document.getElementById('repo-link');
   const warningMessage = document.getElementById('warning-message');
 
-  // Add functionality for the deploy button
+
+  const hideWarningAfterDelay = () => {
+    setTimeout(() => {
+      warningMessage.classList.add('hidden'); 
+      repoLinkInput.value = ""; 
+    }, 2000);
+  };
+
+  
+  
   deployButton.addEventListener('click', () => {
     const repoLink = repoLinkInput.value.trim();
-    warningMessage.classList.add('hidden'); // Hide the warning message initially
-    warningMessage.style.marginTop = "0"; // Reset margin when hidden
-
-    // Clear the input field if there's an issue
-    const clearInput = () => {
-      repoLinkInput.value = ""; // Clear the input field
-    };
+    warningMessage.classList.add('hidden'); 
+    warningMessage.style.marginTop = "0"; 
 
     if (repoLink === "") {
-      warningMessage.textContent = "ᴘʟᴇᴀsᴇ ᴇɴᴛᴇʀ ʏᴏᴜʀ ғᴏʀᴋᴇᴅ ʀᴇᴘᴏ ʟɪɴᴋ.";
-      warningMessage.classList.remove('hidden'); // Show the warning message
-      warningMessage.style.marginTop = "15px"; // Add margin when visible
-      clearInput(); // Clear the input field
+      warningMessage.textContent = "url wrong he .";
+      warningMessage.classList.remove('hidden');
+      warningMessage.style.marginTop = "15px"; 
+      hideWarningAfterDelay(); 
       return;
     }
 
     const githubUrlPattern = /^https:\/\/github\.com\/.+\/.+$/;
 
     if (!githubUrlPattern.test(repoLink)) {
-      warningMessage.textContent = "Pʟᴇᴀsᴇ ᴇɴᴛᴇʀ ᴀ ᴠᴀʟɪᴅ GɪᴛHᴜʙ ʀᴇᴘᴏsɪᴛᴏʀʏ URL.";
-      warningMessage.classList.remove('hidden'); // Show the warning message
-      warningMessage.style.marginTop = "15px"; // Add margin when visible
-      clearInput(); // Clear the input field
-      return; // Exit the function here
+      warningMessage.textContent = "Plaes enter valid url .";
+      warningMessage.classList.remove('hidden'); 
+      warningMessage.style.marginTop = "15px";
+      hideWarningAfterDelay(); 
+      return;
     }
 
-    // Redirect to Heroku deployment page with the GitHub repo URL
-    const herokuUrl = `https://dashboard.heroku.com/new?template=${encodeURIComponent(repoLink)}&env[UPSTREAM_REPO]=${encodeURIComponent(repoLink)}`;
-
+    
+    const herokuUrl = `https://dashboard.heroku.com/new?template=${encodeURIComponent(repoLink)}`;
     window.open(herokuUrl, '_blank');
   });
 });
