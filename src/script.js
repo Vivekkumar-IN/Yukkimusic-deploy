0bf641c66a0f6a55bbc0b09197a2b7a8de3917dd
◄👨‍💻𝑴𝑼𝑹𝑨𝑳𝑰 ✽ 𝑩𝑶𝑻𝒁 ꨄ︎ <148250637+MuraliBotz@users.noreply.github.com>
2024-10-10 12:28:09 +0530

    I think it's works

--- src/script.js
+++ src/script.js
@@ -3,16 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const repoLinkInput = document.getElementById('repo-link');
   const warningMessage = document.getElementById('warning-message');
 
-
-  const hideWarningAfterDelay = () => {
-    setTimeout(() => {
-      warningMessage.classList.add('hidden'); 
-      repoLinkInput.value = ""; 
-    }, 1000000);
-  };
-
-  
-  
   deployButton.addEventListener('click', () => {
     const repoLink = repoLinkInput.value.trim();
     warningMessage.classList.add('hidden'); 
@@ -22,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
       warningMessage.textContent = "Would you mind checking your URL once more? I believe there might be a slight error. 😊🔗..";
       warningMessage.classList.remove('hidden');
       warningMessage.style.marginTop = "15px"; 
-      hideWarningAfterDelay(); 
+      repoLinkInput.value = ""; 
       return;
     }
 
@@ -32,11 +22,10 @@ document.addEventListener("DOMContentLoaded", () => {
       warningMessage.textContent = "It looks like that URL is incorrect..";
       warningMessage.classList.remove('hidden'); 
       warningMessage.style.marginTop = "15px";
-      hideWarningAfterDelay(); 
+      repoLinkInput.value = ""; 
       return;
     }
 
-    
     const herokuUrl = `https://dashboard.heroku.com/new?template=${encodeURIComponent(repoLink)}`;
     window.open(herokuUrl, '_blank');
   });
